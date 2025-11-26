3c61a3c79c2b47fd270588c1ece43b3b28d07112
Alexei <moskvin.lyosha@gmail.com>
2025-11-27 02:00:38 +0300

    Сделал сущность User и операции над ними

---
+++ GetPlaceBackend/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GetPlaceBackend.Models;
+
+public class UserModel
+{
+    public UserModel(string tgid, string username)
+    {
+        TgId = tgid;
+        UserName = username;
+    }
+    
+    [BsonId]
+    public ObjectId UserId { get; set; }
+    public string TgId { get; set; }
+    public string UserName { get; set; }
+    public bool IsDeleted { get; set; } = false;
+}
---
+++ GetPlaceBackend/Services/User/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using GetPlaceBackend.Models;
+
+namespace GetPlaceBackend.Services.User;
+
+public interface IUserService
+{
+    Task<UserModel?> GetById(string tgId);
+    Task<UserModel?> GetByUsername(string username);
+    Task CreateOrUpdate(string tgId, string username);
+}
